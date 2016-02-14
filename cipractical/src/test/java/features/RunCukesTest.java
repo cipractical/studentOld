1d7162d9b4b9b3e32263929c58bea287a5be784b
student1 <student1@example.com>
2016-02-14 23:50:37 +0100

    Added pretty Cucumber reports

--- cipractical/src/test/java/features/RunCukesTest.java
+++ cipractical/src/test/java/features/RunCukesTest.java
@@ -3,6 +3,10 @@ package features;
 import cucumber.api.junit.Cucumber; 
 import org.junit.runner.RunWith; 
 
+import cucumber.api.CucumberOptions;
+
+@CucumberOptions(plugin = {"html:target/cucumber-html-report", 
+"json:target/cucumber-report.json"})
 @RunWith(Cucumber.class)
 
 
