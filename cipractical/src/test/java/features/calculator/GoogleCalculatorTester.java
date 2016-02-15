1a123b9a39450e3592840fd43177f634c9883959
student1 <student1@example.com>
2016-02-15 01:35:13 +0100

    Updated tests to use Selenium WebDriver

--- cipractical/src/test/java/features/calculator/GoogleCalculatorTester.java
+++ cipractical/src/test/java/features/calculator/GoogleCalculatorTester.java
@@ -1,20 +1,37 @@
 package features.calculator;
 
-import calculator.GoogleCalculator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
 
 public class GoogleCalculatorTester {
-	GoogleCalculator myCalc;
+	WebDriver driver;
 	
-	public void launchGoogleCalculator(){
-		myCalc = new GoogleCalculator();
+    public void launchGoogleCalculator(){
+	     driver = new FirefoxDriver();
+		 //Set implicit wait of 5 seconds and launch google
+		 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		 driver.get("https://www.google.co.uk");
 	}
 	
 	public void submitEquation(String equation){
-		myCalc.submitEquation(equation);
+		//Write term in google textbox
+		WebElement googleTextBox = driver.findElement(By.id("lst-ib"));
+		googleTextBox.sendKeys(equation);
+							
+		//Click on searchButton
+		WebElement searchButton = driver.findElement(By.name("btnG"));
+		searchButton.click();
 	}
 	
 	public String getResult(){
-		return (myCalc.getResult());
+		//Get result from calculator
+		WebElement calculatorTextBox = driver.findElement(By.id("cwos"));
+		String actualResult = calculatorTextBox.getText(); 
+		driver.quit();
+		return (actualResult);
 	}
-
+	
 }
